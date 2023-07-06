200c7e4098fcd74ab58a3ab2082a2c05f03a9a59
Alipio Ebenezer Burobe <116289607+alipio37@users.noreply.github.com>
2023-07-06 00:57:15 +0000

    Create sqlsystem1.sql

---
+++ sqlsystem1.sql
@@ -0,0 +1,11 @@
+insert into students (studentname,programid) values ( 'Mary', 'Mad');
+insert into students (studentname,programid) values ( 'john', 'Csat');
+insert into students (studentname,programid) values ( 'Susan', 'Cpct');
+insert into students (studentname,programid) values ( 'Joe', 'Mad');
+select * from students;  
+
+insert into classes (coursename,datetime,instructionid,roomid) values ( 'java', '1 july', 'Alipio', 'BLOG');
+insert into classes (coursename,datetime,instructionid,roomid) values ( 'HTML', '2 july', 'Highway', 'Gaza');
+insert into classes (coursename,datetime,instructionid,roomid) values ( 'CSS', '3 july', 'streets', 'Ssnit');
+insert into classes (coursename,datetime,instructionid,roomid) values ( 'C##', '4 july', 'Burobe', 'Dollar');
+select * from classes;
